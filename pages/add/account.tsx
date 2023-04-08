df42400ca50e70e7eaeade1177e9957aafe6a9c2
Vikehieto <vikehietoyhoshii@gmail.com>
2023-04-08 13:54:00 +0530

    add account to project

--- pages/add/account.tsx
+++ pages/add/account.tsx
@@ -1,33 +1,42 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useAuthContext } from "@/context/AuthContext";
+import { useProjects } from "@/hooks/project";
 
 function CreateAccount() {
-  const [name, setName] = useState("");
-  const [balance, setBalance] = useState("");
-  const [error, setError] = useState("");
   const { user } = useAuthContext();
+  const [name, setName] = useState("");
+  const [balance, setBalance] = useState("0");
+  const { projects } = useProjects(user.id);
+  const [projectId, setProjectId] = useState<number>();
+  const [error, setError] = useState("");
 
   const handleSubmit = async () => {
     if (name.length < 4) {
       setError("Name should be more than 3 characters");
       return;
     }
-    const { data } = await axios.post("/api/account/create", {
+    if (!projectId) {
+      setError("Project is required");
+      return;
+    }
+    const { data } = await axios.post(`/api/user/${user.id}/account/create`, {
       name,
       balance,
-      userId: user.id,
+      projectId,
     });
     console.log(data);
   };
   return (
     <>
+      Name:
       <input
         name="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <br />
+      Balance:
       <input
         name="text"
         value={balance}
@@ -38,6 +47,19 @@ function CreateAccount() {
           setBalance(e.target.value);
         }}
       />
+      <br />
+      Project:
+      <select
+        value={projectId ?? ""}
+        onChange={(e) => setProjectId(Number(e.target.value))}
+      >
+        <option value="">-- Select Project --</option>
+        {projects.map((project) => (
+          <option key={project.id} value={project.id}>
+            {project.name}
+          </option>
+        ))}
+      </select>
       <br />
       {error}
       <br />
--- pages/api/user/[id]/account/create.ts
+++ pages/api/user/[id]/account/create.ts
@@ -6,20 +6,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const { name, balance, projectId } = req.body;
   try {
     const result: MysqlInsert = await db.query(
-      " INSERT INTO account (`name`, `balance`, `userid`) VALUES (?, ?, ? )",
-      [req.body.name, Number(req.body.balance).toFixed(2), req.body.userId]
+      " INSERT INTO account (`name`, `balance`, `project_id`) VALUES (?, ?, ? )",
+      [name, Number(balance).toFixed(2), projectId]
     );
     if (!result.insertId) {
       await db.end();
       return res.status(500).json({ error: "Failed to create category" });
     }
-    const type: Array<any> = await db.query(
-      `SELECT id, name, balance, userid FROM account WHERE id = ${result.insertId}`
+    const account: Array<any> = await db.query(
+      `SELECT id, name, balance, project_id FROM account WHERE id = ${result.insertId}`
     );
     await db.end();
-    res.json({ data: type[0] });
+    res.json({ data: account[0] });
   } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
